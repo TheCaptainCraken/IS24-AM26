e625a5381635a9b5bff6ca2460e0571b44aec885
Pietro Agnoli <87072344+TheCaptainCraken@users.noreply.github.com>
2024-03-05 20:38:22 +0100

    added tests and vscode ignore

---
+++ codex_naturalis/src/test/java/it/polimi/ingsw/ExampleTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Example Test")
+public class ExampleTest {
+    @Test
+    public void exampleTest() {
+        assert true;
+    }
+}
