a4ca86ad655cedfad7ba21ceb9e1410b0ad7f75f
pi <thecaptaincraken@gmail.com>
2024-03-13 17:18:46 +0100

    created simple tests, added every method except constructor.

--- codex_naturalis/src/main/java/it/polimi/ingsw/model/Deck.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/Deck.java
@@ -7,25 +7,44 @@ public class Deck {
     private ArrayList<Card> cards;
 
     public Deck(String nameImport) {
-        //nameImport
-        //json.foreach
-        //switch kind of card aggiungi new ...Card diverse
+        // nameImport
+        // json.foreach
+        // switch kind of card aggiungi new ...Card diverse
         this.shuffle();
     }
 
-    private void shuffle(){
+    /**
+     * This method shuffles the deck.
+     */
+    private void shuffle() {
         Collections.shuffle(cards);
     }
 
-    public Kingdom getKingdomFirstCard(){//per la GUI
+    /**
+     * This method returns the kingdom of the first card of the deck.
+     * 
+     * @return the {@link Kingdom} of the first card of the deck.
+     */
+    public Kingdom getKingdomFirstCard() {
         return cards.get(0).getKingdom();
     }
 
-    public Card draw(){
+    /**
+     * This method returns the first {@link Card} of the deck and then removes it
+     * from the deck.
+     * 
+     * @return the first {@link Card} of the deck.
+     */
+    public Card draw() {
         return cards.remove(0);
     }
 
-    public void insert(Card card){
+    /**
+     * This method adds a {@link Card} to the deck.
+     * 
+     * @param card the {@link Card} to add to the deck.
+     */
+    private void insert(Card card) {
         cards.add(card);
     }
 }
--- codex_naturalis/src/test/java/it/polimi/ingsw/DeckTest.java
+++ codex_naturalis/src/test/java/it/polimi/ingsw/DeckTest.java
@@ -2,6 +2,27 @@ package it.polimi.ingsw;
 
 import org.junit.jupiter.api.Test;
 import it.polimi.ingsw.model.Deck;
+import it.polimi.ingsw.model.Card;
+
+import org.junit.jupiter.api.Assertions;
 
 public class DeckTest {
+    @Test
+    public void notNull() {
+        Deck deck = new Deck("nameImport");
+        Assertions.assertNotNull(deck, "Deck is null");
+    }
+
+    @Test
+    public void draw() {
+        Deck deck = new Deck("nameImport");
+        Card card = deck.draw();
+        Assertions.assertNotNull(card, "Card is null");
+    }
+
+    @Test
+    public void getKingdomFirstCard() {
+        Deck deck = new Deck("nameImport");
+        Assertions.assertNotNull(deck.getKingdomFirstCard(), "Kingdom is null");
+    }
 }
