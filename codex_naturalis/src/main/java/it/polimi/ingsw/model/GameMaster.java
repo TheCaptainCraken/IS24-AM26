e5cb8c3ca3058cda4af3ea77fa690bec909862f9
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2024-03-13 22:33:29 +0100

    Updated pull request
    Branch created from GameMaster branch and not main branch

--- codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.model;
 
-import javafx.scene.effect.Light;
-
 public class GameMaster {
     private int numberOfTurn=0;
     private Lobby community;
@@ -16,91 +14,91 @@ public class GameMaster {
 
 
 
-    void play(){
-        gameSetup();
-        while(notLastTurn){
-            numberOfTurn++;
-            for(i=0; i<numberOfPlayers; i++){
-                notLastTurn=turn();
-            }
-        }
-        for(i=0; i<numberOfPlayers; i++){
-            calculatePointsOfObjective()
-        }
-    }
-
-
-    public void gameSetup(){
-    //using src
-        deckResource = new Deck (...)// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
-        deckGold = new Deck(...)
-    //setup players
-
-
-    //setupTable
-        setOnTableResource(deckResource.draw(), 0)
-        setOnTableResource(deckResource.draw(), 1)
-        setOnTableGold(deck)
-
-
-
-    }
-    //player.drawPlayer(deck.draw())
-
-    //placeCard
-    public int placeCard(String player, PlayableCard cardToPlace, Light.Point position){
-        if(player.equals(currentPlayer)){//TODO capire come identificare user
-            PlayedCard newPlayedCard;
-            if(isPositionable(cardToPlace, )){
-                newPlayedCard=new PlayedCard();
-
-                if(newPlayedCard.getPlayableCard() instanceof SpecialGoldCard){
-                    if(goldSpecialCard.getCountable()=='conta risorse'){
-                        newResourceCard.pointsToAdd(PlayedCard newPlayedCard)
-                    }else{
-                        pointsToAdd(Dictionary currentPlayer.getResources())
-                    }
-                }else{
-                    newPlayedCard.getPlayableCard()
+    /*    void play(){
+            gameSetup();
+            while(notLastTurn){
+                numberOfTurn++;
+                for(i=0; i<numberOfPlayers; i++){
+                    notLastTurn=turn();
                 }
-                //rimuovi carta dalla mano dell-utente
-                player.
-                return 0;
             }
-            return 2;
+            for(i=0; i<numberOfPlayers; i++){
+                calculatePointsOfObjective()
+            }
         }
-        return 1;
-    }
 
-/*    public int drawCard(boolean goldOrNot,){
-        if(goldOrNot){
 
-        }else{
+        public void gameSetup(){
+        //using src
+            deckResource = new Deck (...)// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
+            deckGold = new Deck(...)
+        //setup players
+
+
+        //setupTable
+            setOnTableResource(deckResource.draw(), 0)
+            setOnTableResource(deckResource.draw(), 1)
+            setOnTableGold(deck)
+
+
 
         }
-        //aggiungi a giocatore corrente nello slot vuoto delle carte
-    }
+        //player.drawPlayer(deck.draw())
 
-    private isPositionable(PlayedCard baseCard, Corner, PlayableCard toPlaceCard){
+        //placeCard
+        public int placeCard(String player, Point position){
+            if(player.equals(currentPlayer)){
+                PlayedCard newPlayedCard;
+                if(isPositionable(cardId, )){
+                    newPlayedCard=new PlayedCard();
 
-    }
+                    if(newPlayedCard.getPlayableCard() instanceof SpecialGoldCard){
+                        if(goldSpecialCard.getCountable()=='conta risorse'){
+                            newResourceCard.pointsToAdd(PlayedCard newPlayedCard)
+                        }else{
+                            pointsToAdd(Dictionary currentPlayer.getResources())
+                        }
+                    }else{
+                        newPlayedCard.getPlayableCard()
+                    }
+                    //rimuovi carta dalla mano dell-utente
+                    player.
+                    return 0;
+                }
+                return 2;
+            }
+            return 1;
+        }
 
-    private PlayedCard findCard(PlayedCard card, Point coordinates){
-        if(card==null){
+        public int drawCard(boolean goldOrNot,){
+            if(goldOrNot){
+
+            }else{
+
+            }
+            //aggiungi a giocatore corrente nello slot vuoto delle carte
+        }
+
+        private isPositionable(PlayedCard baseCard, Corner, PlayableCard toPlaceCard){
+
+        }
+
+        private PlayedCard findCard(PlayedCard card, Point coordinates){
+            if(card==null){
+                return null;
+            }else if(card.getCoordinates().x==coordinates.x && card.getCoordinates().y==coordinates.y){
+                return card;
+            }
+            for(Corner corner : Corner.values())
+                PlayedCard found = findCard(card.get, targetCoord);
+                if (found != null) {
+                    return found;
+                }
+            }
             return null;
-        }else if(card.getCoordinates().x==coordinates.x && card.getCoordinates().y==coordinates.y){
-            return card;
         }
-        for(Corner corner : Corner.values())
-            PlayedCard found = findCard(card.get, targetCoord);
-            if (found != null) {
-                return found;
-            }
-        }
-        return null;
-    }
-*/
-    private boolean isPositionable(PlayedCard baseCard,  ){
+    */
+    private boolean isPositionable(PlayedCard playedCard ){
 
     }
 
