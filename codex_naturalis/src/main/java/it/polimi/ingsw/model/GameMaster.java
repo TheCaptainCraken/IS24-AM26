b14024d563b97d780328b94d4c1b19556cc9b1fd
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2024-03-18 12:40:23 +0100

    placeCard, drawCard, isPositionable, findCard, recursiveFindCard, getter and setter, nothing tested

--- codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
@@ -1,97 +1,247 @@
 package it.polimi.ingsw.model;
 import java.awt.*;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.Stack;
 
 public class GameMaster {
-    private int numberOfTurn=0;
-    private Lobby community;
-    String currentPlayer;
-    Deck deckResource;
-    Deck deckGold;
-    Deck deckStart;
-    Deck deckObjective;
-    ResourceCard[] onTableResourceCard;
-    GoldCard[] onTableGoldCard;
-    ObjectiveCard[] onTableObjectiveCard;
+    private int globalTurn=0;//starts from 0
+    //turn /lobby.getPlayers().length-1
+    //currentPlayer %lobby.getPlayers().length
+    private Lobby lobby;
+    //String currentPlayer;
+    private GameState gameState;//TODO non ho capito i gamestate, aggiungere CHOOSING_ROOT_CARD, CHOOSING_OBJECTIVE_CARD
+    private Deck deckResource;//TODO usare i parametri
+    private Deck deckGold;
+    private Deck deckStart;
+    private Deck deckObjective;
+    private ResourceCard[] onTableResourceCard;
+    private GoldCard[] onTableGoldCard;
+    private ObjectiveCard[] onTableObjectiveCard;
 
 
 
-    void play(){
-        gameSetup();
-        while(notLastTurn){
-            numberOfTurn++;
-            for(i=0; i<numberOfPlayers; i++){
-                notLastTurn=turn();
-            }
-        }
-        for(i=0; i<numberOfPlayers; i++){
-            calculatePointsOfObjective()
-        }
-    }
+//    void play(){
+//        gameSetup();
+//        while(notLastTurn){
+//            numberOfTurn++;
+//            for(i=0; i<numberOfPlayers; i++){
+//                notLastTurn=turn();
+//            }
+//        }
+//        for(i=0; i<numberOfPlayers; i++){
+//            calculatePointsOfObjective()
+//        }
+//    }
 
 
- /*   public void gameSetup(){
+    public void gameSetup(){//TODO
     //using src
-        deckResource = new Deck (...)// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
+        /*deckResource = new Deck (...)// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
         deckGold = new Deck(...)
     //setup players
-
+        deckObjective = new Deck(...);
+        deckStart = new Deck(...);
+        for(Player player : lobby.getPlayers()){
+            //player.setRootCard(deckStart.draw());
+        }
+        for(Player player : lobby.getPlayers()){
+            player.setRootCard(deckStart.draw());
+        }
 
     //setupTable
-        setOnTableResource(deckResource.draw(), 0)
-        setOnTableResource(deckResource.draw(), 1)
-        setOnTableGold(deck)
+        setOnTableResource(deckResource.draw(), 0);
+        setOnTableResource(deckResource.draw(), 1);
+        setOnTableGold(deck);*/
 
 
 
     }
     //player.drawPlayer(deck.draw())
 
-    //placeCard
-    public int placeCard(String player, PlayableCard cardToPlace, Point position){
-        if(player.equals(currentPlayer)){//TODO capire come identificare user
-            PlayedCard newPlayedCard;
-            if(isPositionable(cardToPlace, )){
-                newPlayedCard=new PlayedCard();
+    //TODO pick starting cart e objective card
+    public int placeRootCard(String nicknamePlayer){//TODO devo pescare la carta fuori da qua e mostrarla con un-altra funzione e poi settarla qui, capire dove metterla nel mentre, due array (un array e una matrice alta due e num gioc)
+        Player currentPlayer = getCurrentPlayer();
+        if(isCurrentPlayer(nicknamePlayer, currentPlayer)) {
+            if (gameState == GameState.CHOOSING_ROOT_CARD) {
 
-                if(newPlayedCard.getPlayableCard() instanceof SpecialGoldCard){
-                    if(goldSpecialCard.getCountable()=='conta risorse'){
-                        newResourceCard.pointsToAdd(PlayedCard newPlayedCard)
-                    }else{
-                        pointsToAdd(Dictionary currentPlayer.getResources())
+                currentPlayer.setRootCard(deckStart.draw());
+            }
+        }
+        return 1;//is not their turn
+    }
+
+    public int chooseObjectiveCard(String nicknamePlayer){
+        Player currentPlayer = getCurrentPlayer();
+        if(isCurrentPlayer(nicknamePlayer, currentPlayer)) {
+            if (gameState == GameState.CHOOSING_OBJECTIVE_CARD) {
+
+            }
+        }
+        return 1;//is not their turn
+    }
+
+    //placeCard
+    public int placeCard(String nicknamePlayer, PlayableCard cardToPlace, Point coordinates, boolean side){
+        Player currentPlayer = getCurrentPlayer();
+        if(isCurrentPlayer(nicknamePlayer, currentPlayer)){
+            if(gameState==GameState.PLACING_PHASE) {
+                Dictionary<Corner, PlayedCard> attachments = isPositionable(currentPlayer.getRootCard(), cardToPlace, coordinates);
+                if (attachments != null) {
+                    if (cardToPlace instanceof GoldCard) {
+                        GoldCard goldCard = (SpecialGoldCard) cardToPlace;
+                        if (!goldCard.requirementsSatisfied(currentPlayer.getSimbolCounter())) {
+                            return 4;//not enough resources
+                        }
                     }
-                }else{
-                    newPlayedCard.getPlayableCard()
+                    PlayedCard newPlayedCard = new PlayedCard(cardToPlace, attachments, side, getTurn(), coordinates);//the attachments are of the graph of the player who is playing so there isn-t any reference to Player class in the constructor
+                    if (cardToPlace instanceof SpecialGoldCard) {
+                        SpecialGoldCard specialGoldCard = (SpecialGoldCard) cardToPlace;
+                        if (specialGoldCard.getCountable() == Countable.CORNER) {
+                            currentPlayer.addPoints(specialGoldCard.pointsToAdd(newPlayedCard));
+                        } else {
+                            currentPlayer.addPoints(specialGoldCard.pointsToAdd(currentPlayer.getResources()));
+                        }
+                    } else {
+                        ResourceCard resourceCard = (ResourceCard) newPlayedCard.getPlayableCard();
+                        currentPlayer.addPoints(resourceCard.getPoints());
+                    }
+                    //rimuovi carta dalla mano dell-utente
+                    currentPlayer.removePlayedCard(cardToPlace.getId());
+                    gameState=GameState.DRAWING_PHASE;
+                    return 0;
                 }
-                //rimuovi carta dalla mano dell-utente
-                player.
+                return 3;//is not positionable
+            }
+            return 2;//is not the right phase
+        }
+        return 1;//is not their turn
+    }
+
+    public int drawCard(String nicknamePlayer, boolean goldOrNot, int onTableOrDeck){ //onTableOrDeck has 0, 1 for position of array of cards on table and 2 for draw from deck
+        Player currentPlayer = getCurrentPlayer();
+        if(isCurrentPlayer(nicknamePlayer, currentPlayer)) {//TODO mazzo finito
+            if(gameState==GameState.DRAWING_PHASE){
+                if (goldOrNot) {
+                    if (onTableOrDeck == 2) {//TODO la view richiede il nuovo retro di quella in cima e non solo di quella spostata
+                        currentPlayer.draw(deckGold.draw());
+                    }else{
+                        currentPlayer.draw(onTableGoldCard[onTableOrDeck]);
+                        onTableGoldCard[onTableOrDeck]=deckGold.draw();
+                    }
+                } else {
+                    if (onTableOrDeck == 2) {//TODO la view richiede il nuovo retro di quella in cima e non solo di quella spostata
+                        currentPlayer.draw(deckResource.draw());
+                    }else{
+                        currentPlayer.draw(onTableResourceCard[onTableOrDeck]);
+                        onTableResourceCard[onTableOrDeck]=deckResource.draw();
+                    }
+                }
+                gameState=GameState.PLACING_PHASE;
+                nextGlobalTurn();
                 return 0;
             }
-            return 2;
+            return 2;//is not the right phase
         }
-        return 1;
+        return 1;//is not their turn
     }
 
-    public int drawCard(boolean goldOrNot,){
-        if(goldOrNot){
+    //TODO calcolaObiettiviAllaFine
 
-        }else{
 
+    //notation is x and y based on cartesian axes system rotated of 45 degrees counterclockwise, every card represents a dot with natural coordinates
+    //example: starting card is always 0,0 so TOP_LEFT would be 0;1, TOP_RIGHT
+    private Dictionary<Corner, PlayedCard> isPositionable(PlayedCard startingCard, PlayableCard cardToPlace, Point coordinates/*, Corner cornerOfCardToPlace*/){
+        Dictionary<Corner, PlayedCard> attachments = new Hashtable<>();
+        PlayedCard cardToCheck;
+//        int xBaseToPlace=0, yBaseToPlace=0;
+        int xPlaceToCheck=0, yPlaceToCheck=0;
+//        switch(cornerOfCardToPlace){
+//            case TOP_LEFT:{
+//                xBaseToPlace=0;
+//                yBaseToPlace=-1;
+//                break;
+//            }
+//            case TOP_RIGHT:{
+//                xBaseToPlace=-1;
+//                yBaseToPlace=0;
+//                break;
+//            }
+//            case BOTTOM_LEFT:{
+//                xBaseToPlace=1;
+//                yBaseToPlace=0;
+//                break;
+//            }
+//            case BOTTOM_RIGHT:{
+//                xBaseToPlace=0;
+//                yBaseToPlace=1;
+//                break;
+//            }
+//        }
+        for(Corner corner : Corner.values()){
+            switch(corner){
+                case TOP_LEFT:{
+                    xPlaceToCheck=0;
+                    yPlaceToCheck=1;
+                    break;
+                }
+                case TOP_RIGHT:{
+                    xPlaceToCheck=1;
+                    yPlaceToCheck=0;
+                    break;
+                }
+                case BOTTOM_LEFT:{
+                    xPlaceToCheck=-1;
+                    yPlaceToCheck=0;
+                    break;
+                }
+                case BOTTOM_RIGHT:{
+                    xPlaceToCheck=0;
+                    yPlaceToCheck=-1;
+                    break;
+                }
+            }
+            cardToCheck = findCard(startingCard, new Point(coordinates.x+xPlaceToCheck, coordinates.y+yPlaceToCheck));
+            //if(corner!=cornerOfCardToPlace){
+            //    cardToCheck = findCard(baseCard, new Point(baseCard.getCoordinates().x+xBaseToPlace+xPlaceToCheck, baseCard.getCoordinates().y+yBaseToPlace+yPlaceToCheck));
+            //}else{
+            //    cardToCheck = baseCard;
+            //}
+            if(cardToCheck!=null){
+                Sign cornerToCheck;
+                switch(corner){
+                    case TOP_LEFT:{
+                       cornerToCheck=cardToCheck.getPlayableCard().getCorners().get(Corner.BOTTOM_RIGHT);
+                        break;
+                    }
+                    case TOP_RIGHT:{
+                        cornerToCheck=cardToCheck.getPlayableCard().getCorners().get(Corner.BOTTOM_LEFT);
+                        break;
+                    }
+                    case BOTTOM_LEFT:{
+                        cornerToCheck=cardToCheck.getPlayableCard().getCorners().get(Corner.TOP_RIGHT);
+                        break;
+                    }
+                    case BOTTOM_RIGHT:{
+                        cornerToCheck=cardToCheck.getPlayableCard().getCorners().get(Corner.TOP_LEFT);
+                        break;
+                    }
+                }
+                if(cornerToCheck==null){
+                    return null;
+                }
+            }
+            attachments.put(corner, cardToCheck);
         }
-        //aggiungi a giocatore corrente nello slot vuoto delle carte
+        return attachments;
     }
-
-    private isPositionable(PlayedCard baseCard, Corner, PlayableCard toPlaceCard){
-
-    }
-*/
 
     private PlayedCard findCard(PlayedCard card, Point coordinates){
-        Stack<PlayedCard> stack = new Stack<PlayedCard>();//Anche Arraylist è uguale
+        Stack<PlayedCard> stack = new Stack<>();//Anche Arraylist è uguale
         return recursiveFindCard(card, coordinates, stack);
     }
 
-    private PlayedCard recursiveFindCard(PlayedCard card, Point coordinates, Stack stack) {
+    private PlayedCard recursiveFindCard(PlayedCard card, Point coordinates, Stack<PlayedCard> stack) {
         if (stack.search(card) > 0 || card == null) {
             return null;
         } else if (card.getCoordinates().x == coordinates.x && card.getCoordinates().y == coordinates.y) {
@@ -106,12 +256,20 @@ public class GameMaster {
         return null;
     }
 
-    private boolean isPositionable(PlayedCard baseCard, Corner angleOfBaseCard, PlayableCard ){
-
+    public int getTurn(){
+        return globalTurn / lobby.getPlayers().length - 1 ;//non conta giro rootCard e giro ObjectiveCard
     }
 
-    public void setCurrentPlayer(String currentPlayer) {
-        this.currentPlayer = currentPlayer;
+    public Player getCurrentPlayer(){
+        return lobby.getPlayers()[globalTurn % lobby.getPlayers().length];
+    }
+
+    public boolean isCurrentPlayer(String nicknamePlayer, Player currentPlayer){
+        nicknamePlayer.equals(currentPlayer.getNickname());
+    }
+
+    public void nextGlobalTurn() {
+        this.globalTurn++;
     }
 
     public void setOnTableResourceCard(ResourceCard[] onTableResourceCard) {
@@ -124,5 +282,17 @@ public class GameMaster {
 
     public void setOnTableObjectiveCard(ObjectiveCard[] onTableObjectiveCard) {
         this.onTableObjectiveCard = onTableObjectiveCard;
+    }
+
+    public int getPlayerPoints(){
+
+    }
+
+    public Dictionary<Sign, Integer> getPlayerResources(){
+
+    }
+
+    public ResourceCard[] getPlayerHand(){
+
     }
 }
--- codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
@@ -4,16 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lobby {
-    private List<Player> community;
+    private List<Player> players;
 
     public Lobby(){
-        community = new ArrayList<Player>();
+        players = new ArrayList<Player>();
     }
-    public Player addPlayer(String nickname, ColorPin colorPin){//TODO check not more then 4
-        community.add(new Player(nickname, community.size()==0, community.size()+1));
+    public Player addPlayer(String nickname/*, ColorPin colorPin*/){
+        if(players.size()<4){
+            Player newPlayer = new Player(nickname, ColorPin.values()[players.size()], players.size()==0);
+            players.add(newPlayer);
+            return newPlayer;
+        }
+        return null;
     }
 
-    public List<Player> getCommunity() {
-        return community;
+    public Player[] getPlayers() {
+        return players.toArray( new Player[players.size()]);
     }
 }
