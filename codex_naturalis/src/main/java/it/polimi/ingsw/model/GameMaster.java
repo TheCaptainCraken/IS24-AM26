2b65e1e39cfb6b8352f3569824c6542109f66858
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2024-03-19 18:14:47 +0100

    Switched from HashTable to HashMap, moved getPlayerFromName ,from GameMaster to Lobby changed name of some methods following UML

--- codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/GameMaster.java
@@ -1,50 +1,51 @@
 package it.polimi.ingsw.model;
 import java.awt.*;
-import java.util.HashTable;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.HashMap;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class GameMaster {
-    private int globalTurn=0;//starts from 0
+    private int globalTurn;//starts from 0
     //turn /lobby.getPlayers().length-1
     //currentPlayer %lobby.getPlayers().length
-    private int turnRemained;
+    private int flagTurnRemained;
     private Lobby lobby;
     //String currentPlayer;
     private GameState gameState;//TODO non ho capito i gamestate, aggiungere CHOOSING_ROOT_CARD, CHOOSING_OBJECTIVE_CARD
-    private Deck deckResource;//TODO usare i parametri
-    private Deck deckGold;
-    private Deck deckStart;
-    private Deck deckObjective;
-    private ResourceCard[] onTableResourceCard;
-    private GoldCard[] onTableGoldCard;
-    private ObjectiveCard[] onTableObjectiveCard;
+    private Deck resourceDeck;//TODO usare i parametri
+    private Deck goldDeck;
+    private Deck startingDeck;
+    private Deck objectiveDeck;
+    private ResourceCard[] onTableResourceCards;
+    private GoldCard[] onTableGoldCards;
+    private ObjectiveCard[] onTableObjectiveCards;
     private StartingCard[] startingCardToPosition;
     private ObjectiveCard[][] objectiveCardToChoose;
 
     public GameMaster(Lobby lobby, String jsonResourceCardFileName,  String jsonGoldCardFileName,  String jsonObjectiveCardFileName,  String jsonObjectiveStartFileName) {
         this.globalTurn = 0;
-        this.turnRemained = 2; //is a value to say is not the last, it could be substituted with ENUM: PLAYING, SECOND-LAST_TURN, LAST_TURN
+        this.flagTurnRemained = 2; //is a value to say is not the last, it could be substituted with ENUM: PLAYING, SECOND-LAST_TURN, LAST_TURN
         this.lobby = lobby;
         this.gameState = GameState.CHOOSING_ROOT_CARD;
-        this.deckResource = new Deck (jsonResourceCardFileName);// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
-        this.deckGold = new Deck(jsonGoldCardFileName);
-        this.deckObjective = new Deck(jsonObjectiveCardFileName);
-        this.deckStart = new Deck(jsonObjectiveStartFileName);
+        this.resourceDeck = new Deck (jsonResourceCardFileName);// mettiamo direttamente il nome del json e poi calcoliamo quanto è grande?
+        this.goldDeck = new Deck(jsonGoldCardFileName);
+        this.objectiveDeck = new Deck(jsonObjectiveCardFileName);
+        this.startingDeck = new Deck(jsonObjectiveStartFileName);
         //setupTable
-        setOnTableResourceCard(deckResource.draw(), 0);
-        setOnTableResourceCard(deckResource.draw(), 1);
-        setOnTableGoldCard(deckGold.draw(), 0);
-        setOnTableGoldCard(deckGold.draw(), 1);
+        setOnTableResourceCard(resourceDeck.draw(), 0);
+        setOnTableResourceCard(resourceDeck.draw(), 1);
+        setOnTableGoldCard(goldDeck.draw(), 0);
+        setOnTableGoldCard(goldDeck.draw(), 1);
         for(Player player : lobby.getPlayers()){
             player.setStartingHand();
         }
         for(int i=0; i<lobby.getPlayers().length; i++){
-            startingCardToPosition[i]=(StartingCard) deckStart.draw();
+            startingCardToPosition[i]=(StartingCard) startingDeck.draw();
         }
         for(int i=0; i<lobby.getPlayers().length; i++){
             for(int j=0; j<2; j++){
-                objectiveCardToChoose[i][j]=(ObjectiveCard) deckObjective.draw();
+                objectiveCardToChoose[i][j]=(ObjectiveCard) objectiveDeck.draw();
             }
         }
     }
@@ -64,20 +65,20 @@ public class GameMaster {
 
 
     //TODO pick starting cart e objective card
-    public int placeRootCard(String nicknamePlayer, boolean side){//TODO devo pescare la carta fuori da qua e mostrarla con un-altra funzione e poi settarla qui, capire dove metterla nel mentre, due array (un array e una matrice alta due e num gioc)
+    public int placeRootCard(String namePlayer, boolean side){//TODO devo pescare la carta fuori da qua e mostrarla con un-altra funzione e poi settarla qui, capire dove metterla nel mentre, due array (un array e una matrice alta due e num gioc)
         Player currentPlayer = getCurrentPlayer();
-        if(!isCurrentPlayer(nicknamePlayer, currentPlayer)) {
+        if(!isCurrentPlayer(namePlayer, currentPlayer)) {
             throw new IllegalStateException ("It's not the turn of this player");//is not their turn
         }else if(gameState != GameState.CHOOSING_ROOT_CARD) {
             throw new IllegalStateException ("It's not the right phase to choose how to place the card");
         }else{
-            HashTable<Corner, PlayedCard>  defaultAttachments = new HashTable<>();
+            HashMap<Corner, PlayedCard>  defaultAttachments = new HashMap<>();
             for(Corner corner : Corner.values()){
                 defaultAttachments.put(corner, null);
             }
-            currentPlayer.setRootCard(new PlayedCard(startingCardToPosition[getOrderOfPlayOfThePlayer(currentPlayer.getNickname())], defaultAttachments, side, 0, new Point(0, 0)));
+            currentPlayer.setRootCard(new PlayedCard(startingCardToPosition[getOrderOfPlayOfThePlayer(currentPlayer.getName())], defaultAttachments, side, 0, new Point(0, 0)));
             nextGlobalTurn();
-            if(getOrderOfPlayOfThePlayer(getCurrentPlayer().getNickname())==0){
+            if(getOrderOfPlayOfThePlayer(getCurrentPlayer().getName())==0){
                 gameState=GameState.CHOOSING_OBJECTIVE_CARD;
             }
             return 0;
@@ -85,16 +86,16 @@ public class GameMaster {
         return 1;
     }
 
-    public int chooseObjectiveCard(String nicknamePlayer, int whichCard){
+    public int chooseObjectiveCard(String namePlayer, int whichCard){
         Player currentPlayer = getCurrentPlayer();
-        if(!isCurrentPlayer(nicknamePlayer, currentPlayer)) {
+        if(!isCurrentPlayer(namePlayer, currentPlayer)) {
             throw new IllegalStateException ("It's not the turn of this player");//is not their turn
         }else if(gameState != GameState.CHOOSING_OBJECTIVE_CARD) {
             throw new IllegalStateException ("It's not the right phase to choose how to place the card");
         }else{
-            currentPlayer.setSecretObjective(objectiveCardToChoose[getOrderOfPlayOfThePlayer(currentPlayer.getNickname())][whichCard]);
+            currentPlayer.setSecretObjective(objectiveCardToChoose[getOrderOfPlayOfThePlayer(currentPlayer.getName())][whichCard]);
             nextGlobalTurn();
-            if(getOrderOfPlayOfThePlayer(getCurrentPlayer().getNickname())==0){
+            if(getOrderOfPlayOfThePlayer(getCurrentPlayer().getName())==0){
                 gameState=GameState.PLACING_PHASE;
             }
             return 0;
@@ -103,11 +104,11 @@ public class GameMaster {
     }
 
     //placeCard
-    public int placeCard(String nicknamePlayer, PlayableCard cardToPlace, Point coordinates, boolean side){
+    public int placeCard(String namePlayer, PlayableCard cardToPlace, Point coordinates, boolean side){
         Player currentPlayer = getCurrentPlayer();
-        if(isCurrentPlayer(nicknamePlayer, currentPlayer)){
+        if(isCurrentPlayer(namePlayer, currentPlayer)){
             if(gameState==GameState.PLACING_PHASE) {
-                HashTable<Corner, PlayedCard> attachments = isPositionable(currentPlayer.getRootCard(), cardToPlace, coordinates);
+                HashMap<Corner, PlayedCard> attachments = isPositionable(currentPlayer.getRootCard(), cardToPlace, coordinates);
                 if (attachments != null) {
                     if(side){
                         if (cardToPlace instanceof GoldCard) {
@@ -177,34 +178,34 @@ public class GameMaster {
         return null;//TODO excpetion
     }
 
-    public int drawCard(String nicknamePlayer, boolean goldOrNot, int onTableOrDeck){ //onTableOrDeck has 0, 1 for position of array of cards on table and 2 for draw from deck
+    public ResourceCard drawCard(String namePlayer, boolean goldOrNot, int onTableOrDeck){ //onTableOrDeck has 0, 1 for position of array of cards on table and 2 for draw from deck
         Player currentPlayer = getCurrentPlayer();
-        if(isCurrentPlayer(nicknamePlayer, currentPlayer)) {//TODO mazzo finito
+        if(isCurrentPlayer(namePlayer, currentPlayer)) {//TODO mazzo finito
             if(gameState==GameState.DRAWING_PHASE){
                 if (goldOrNot) {
                     if (onTableOrDeck == 2) {//TODO la view richiede il nuovo retro di quella in cima e non solo di quella spostata
-                        currentPlayer.giveCard((PlayableCard) deckGold.draw());
+                        currentPlayer.giveCard((PlayableCard) goldDeck.draw());
                     }else{
-                        currentPlayer.giveCard(onTableGoldCard[onTableOrDeck]);
-                        onTableGoldCard[onTableOrDeck]=deckGold.draw();
+                        currentPlayer.giveCard(onTableGoldCards[onTableOrDeck]);
+                        onTableGoldCards[onTableOrDeck]= goldDeck.draw();
                     }
                 } else {
                     if (onTableOrDeck == 2) {//TODO la view richiede il nuovo retro di quella in cima e non solo di quella spostata
-                        currentPlayer.giveCard(deckResource.draw());
+                        currentPlayer.giveCard(resourceDeck.draw());
                     }else{
-                        currentPlayer.giveCard(onTableResourceCard[onTableOrDeck]);
-                        onTableResourceCard[onTableOrDeck]=deckResource.draw();
+                        currentPlayer.giveCard(onTableResourceCards[onTableOrDeck]);
+                        onTableResourceCards[onTableOrDeck]= resourceDeck.draw();
                     }
                 }
                 //next player will play?
                 //TODO maybe it could be written in a better way
-                if(turnRemained==1&&getOrderOfPlayOfThePlayer(currentPlayer.getNickname())+1==lobby.getPlayers().length){//if it is the last player in second-last turn cicle, say the next is the last turn
-                    turnRemained=0;
+                if(flagTurnRemained ==1&&getOrderOfPlayOfThePlayer(currentPlayer.getName())+1==lobby.getPlayers().length){//if it is the last player in second-last turn cicle, say the next is the last turn
+                    flagTurnRemained =0;
                     gameState = GameState.PLACING_PHASE;
-                } else if (turnRemained==0&&getOrderOfPlayOfThePlayer(currentPlayer.getNickname())+1==lobby.getPlayers().length) {//if it is the last player in last turn cicle, go to end mode
+                } else if (flagTurnRemained ==0&&getOrderOfPlayOfThePlayer(currentPlayer.getName())+1==lobby.getPlayers().length) {//if it is the last player in last turn cicle, go to end mode
                     gameState=GameState.END;
                 }else if(currentPlayer.getPoints()>=20){//if a player reached 20 points set this turn cicle as the second-last
-                    turnRemained=1;
+                    flagTurnRemained =1;
                     gameState = GameState.PLACING_PHASE;
                 }else{
                     gameState = GameState.PLACING_PHASE;
@@ -218,16 +219,16 @@ public class GameMaster {
     }
 
     //TODO calcolaObiettiviAllaFine
-    public int endGame(String nicknamePlayer){ //Player will click a button to calculate their points
+    public int endGame(String namePlayer){ //Player will click a button to calculate their points
         Player currentPlayer = getCurrentPlayer();
-        if(!isCurrentPlayer(nicknamePlayer, currentPlayer)) {
+        if(!isCurrentPlayer(namePlayer, currentPlayer)) {
             throw new IllegalStateException ("It's not the turn of this player");//is not their turn
         }else if(gameState != GameState.END) {
             throw new IllegalStateException ("It's not the right phase to choose how to place the card");
         }else{
             currentPlayer.addPoints(currentPlayer.getSecretObjective().effect(currentPlayer.getRootCard()));//TODO finire, qua fare con l'overload dell'interfaccia e poi in questo metodo si chiameranno i metodi di ricerca di this
-            for(ObjectiveCard objectiveCard : onTableObjectiveCard){
-                currentPlayer.addPoints(onTableObjectiveCard.effect(currentPlayer.getRootCard()));//
+            for(ObjectiveCard objectiveCard : onTableObjectiveCards){
+                currentPlayer.addPoints(onTableObjectiveCards.effect(currentPlayer.getRootCard()));//
             }
             return currentPlayer.getPoints();
         }
@@ -237,8 +238,8 @@ public class GameMaster {
 
     //notation is x and y based on cartesian axes system rotated of 45 degrees counterclockwise, every card represents a dot with natural coordinates
     //example: starting card is always 0,0 so TOP_LEFT would be 0;1, TOP_RIGHT
-    private HashTable<Corner, PlayedCard> isPositionable(PlayedCard startingCard, PlayableCard cardToPlace, Point coordinates/*, Corner cornerOfCardToPlace*/){
-        HashTable<Corner, PlayedCard> attachments = new Hashtable<>();
+    private HashMap<Corner, PlayedCard> isPositionable(PlayedCard startingCard, PlayableCard cardToPlace, Point coordinates/*, Corner cornerOfCardToPlace*/){
+        HashMap<Corner, PlayedCard> attachments = new HashMap<>();
         PlayedCard cardToCheck;
 //        int xBaseToPlace=0, yBaseToPlace=0;
         int xPlaceToCheck=0, yPlaceToCheck=0;
@@ -345,7 +346,7 @@ public class GameMaster {
         return attachments;
     }
 
-    private PlayedCard findCard(PlayedCard card, Point coordinates){//TODO METTERLO A DISPOSIZIONE DEL MODULE DI GOLDCARD E OBJECTCARD
+    private PlayedCard findCard(PlayedCard startingCard, Point coordinates){//TODO METTERLO A DISPOSIZIONE DEL MODULE DI GOLDCARD E OBJECTCARD
         Stack<PlayedCard> stack = new Stack<>();//Anche Arraylist è uguale
         return recursiveFindCard(card, coordinates, stack);
     }
@@ -373,26 +374,17 @@ public class GameMaster {
         return lobby.getPlayers()[globalTurn % lobby.getPlayers().length];
     }
 
-    public Player getPlayerFromNickname(String nickname){
-        for(Player player : lobby.getPlayers()){
-            if(player.getNickname().equals(nickname)){
-                return player;
-            }
-        }
-        return null;//TODO add a NoSuchFieldException
-    }
-
-    public int getOrderOfPlayOfThePlayer(String nickname){
+    public int getOrderOfPlayOfThePlayer(String name){
         for(int i=0; i<lobby.getPlayers().length; i++){
-            if(lobby.getPlayers()[i].getNickname().equals(nickname)){
+            if(lobby.getPlayers()[i].getName().equals(name)){
                 return i;
             }
         }
         return -1;//TODO add a NoSuchFieldException
     }
 
-    public boolean isCurrentPlayer(String nicknamePlayer, Player currentPlayer){
-        nicknamePlayer.equals(currentPlayer.getNickname());
+    public boolean isCurrentPlayer(String namePlayer, Player currentPlayer){
+        return namePlayer.equals(currentPlayer.getName());
     }
 
     public void nextGlobalTurn() {
@@ -400,34 +392,38 @@ public class GameMaster {
     }
 
     public void setOnTableResourceCard(ResourceCard resourceCard, int place) {
-        this.onTableResourceCard[place] = resourceCard;
+        this.onTableResourceCards[place] = resourceCard;
     }
 
     public void setOnTableGoldCard(GoldCard goldCard, int place) {
-        this.onTableGoldCard[place] = goldCard;
+        this.onTableGoldCards[place] = goldCard;
     }
 
-    public void setOnTableObjectiveCard(ObjectiveCard[] onTableObjectiveCard) {
-        this.onTableObjectiveCard = onTableObjectiveCard;
+    public void setOnTableObjectiveCards(ObjectiveCard[] onTableObjectiveCards) {//TODO modificare come quelli prima
+        this.onTableObjectiveCards = onTableObjectiveCards;
     }
 
-    public int getPlayerPoints(String nickname){
-        return getPlayerFromNickname(nickname).getPoints();
+    public int getPlayerPoints(String name){
+        return lobby.getPlayerFromName(name).getPoints();
+    }
+
+    public HashMap<Sign, Integer> getPlayerResources(String name){
+        return lobby.getPlayerFromName(name).getSimbolCounter();
     }
 
 //    public StartingCard[] getStartingCardToPosition() {
 //        return startingCardToPosition;
 //    }
 //
-//    public ObjectiveCard[][] getObjectiveCardToChoose(String nickname) {
+//    public ObjectiveCard[][] getObjectiveCardToChoose(String name) {
 //        return objectiveCardToChoose[];
 //    }
 //
-//    public HashTable<Sign, Integer> getPlayerResources(){
+//    public HashMap<Sign, Integer> getPlayerResources(){
 //
 //    }
 //
-//    public ResourceCard[] getPlayerHand(String nickname){
+//    public ResourceCard[] getPlayerHand(String name){
 //        get
 //    }
 }
--- codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
@@ -4,21 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lobby {
-    private List<Player> players;
+    private ArrayList<Player> players;
 
     public Lobby(){
-        players = new ArrayList<Player>();
+        players = new ArrayList<>();
     }
-    public Player addPlayer(String nickname/*, ColorPin colorPin*/){
+    public void addPlayer(String nickname/*, ColorPin colorPin*/){//TODO excpetion
         if(players.size()<4){
             Player newPlayer = new Player(nickname, ColorPin.values()[players.size()], players.size()==0);
             players.add(newPlayer);
-            return newPlayer;
+            //return newPlayer;
         }
-        return null;
+        //return null;
     }
 
     public Player[] getPlayers() {
         return players.toArray( new Player[players.size()]);
+    }
+
+    public Player getPlayerFromName(String nickname){//TODO clone
+        for(Player player : getPlayers()){
+            if(player.getNickname().equals(nickname)){
+                return player;
+            }
+        }
+        return null;//TODO add a NoSuchFieldException
     }
 }
