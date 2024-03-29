1af7c23d04b61e45953e0aa660a127c96c93b279
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2024-03-29 23:31:50 +0100

    Updates to Lobby
    Added check of unique nickname in addPlayer() method
    Added Javadoc
    Corrected boolean complete initial value and method setLock()

--- codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/model/Lobby.java
@@ -5,14 +5,26 @@ import java.util.List;
 
 public class Lobby {
     private ArrayList<Player> players;
-    boolean complete=true;
+    boolean complete=false;
 
+    /**It's an holder for the players that permits to check and limit access
+     */
     public Lobby(){
         players = new ArrayList<>();
     }
-    public void addPlayer(String nickname, Color color){//TODO excpetion
+
+    /**If possible it adds a new player to the lobby with an unique nickname
+     * @param nickname nickname of the new Player
+     * @param color color of the pin that the player has chosen
+     */
+    public void addPlayer(String nickname, Color color) throws IndexOutOfBoundsException, IllegalArgumentException{//TODO excpetion
         if(complete){
             throw new IndexOutOfBoundsException("The lobby has already 4 players");
+        }
+        for(Player player : players){
+            if(player.getName().equals(nickname)){
+                throw new IllegalArgumentException("There's already a player with this nickname");
+            }
         }
         Player newPlayer = new Player(nickname, color);
         players.add(newPlayer);
@@ -21,20 +33,29 @@ public class Lobby {
         }
     }
 
+    /**Get a fixed array of players
+     * @return an array of players
+     */
     public Player[] getPlayers() {
         return players.toArray( new Player[players.size()]);
     }
 
-    public Player getPlayerFromName(String nickname){//TODO clone
+    /**Givena  nickname it returns the player with that unique nickname
+     * @param nickname nickname of the Player we want to get
+     * @return the Player with the given nickname
+     */
+    public Player getPlayerFromName(String nickname) throws NoSuchFieldException {//TODO clone
         for(Player player : getPlayers()){
             if(player.getName().equals(nickname)){
                 return player;
             }
         }
-        return null;//TODO add a NoSuchFieldException
+        throw new NoSuchFieldException("There's no Player with the given nickname");
     }
 
+    /** It locks the lobby so nobody can join anymore, the lobby cannot be unlocked
+     */
     public void setLock(){
-        complete=false;
+        complete=true;
     }
 }
