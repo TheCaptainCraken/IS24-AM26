788136ec66235c7d37ec49b3e0b3c0b8fb7af287
pietrobenecchi <pietro.benecchi@gmail.com>
2024-03-29 09:32:35 +0100

    merge from main to update pom

--- codex_naturalis/src/test/java/it/polimi/ingsw/GameMasterTest.java
+++ codex_naturalis/src/test/java/it/polimi/ingsw/GameMasterTest.java
@@ -2,7 +2,9 @@ package it.polimi.ingsw;
 
 import org.junit.jupiter.api.Test;
 import it.polimi.ingsw.model.GameMaster;
+import static org.mockito.Mockito.*;
 
 public class GameMasterTest {
+    GameMaster master = mock(GameMaster.class);
 
 }
