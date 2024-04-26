c9022a1f5b5b216f8045026f1a07347e48768b3d
Daniel Carrozzo <daniel.carrozzo@gmail.com>
2024-04-26 11:37:35 +0200

    Removed Server file from GameMaster branch

--- codex_naturalis/src/main/java/it/polimi/ingsw/server/server/Loggable.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw.server.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Loggable extends Remote {
-    Boolean ping() throws RemoteException;
-}
