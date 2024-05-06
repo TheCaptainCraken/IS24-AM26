aac939ac7af0abc39275769b9850b32bc8b67b49
Artibugi0 <arturo.amoretti@mail.polimi.it>
2024-05-06 18:17:59 +0200

    Converted InterfaceClient into a Java Interface

--- codex_naturalis/src/main/java/it/polimi/ingsw/network/client/ClientRMI.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/network/client/ClientRMI.java
@@ -19,7 +19,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.HashMap;
 
-public class ClientRMI extends InterfaceClient {
+public class ClientRMI implements InterfaceClient {
     int PORT = 1234;
     Controller controller;
     //TODO check if is to do a singletone
