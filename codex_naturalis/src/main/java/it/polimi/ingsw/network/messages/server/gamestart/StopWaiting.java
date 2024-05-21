d7802e8dd972fb731194d39c70149f2b1ff82fd2
Pietro Agnoli <pietro1.agnoli@mail.polimi.it>
2024-05-21 13:04:06 +0200

    almost done with socket

---
+++ codex_naturalis/src/main/java/it/polimi/ingsw/network/messages/server/gamestart/StopWaiting.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.network.messages.server.gamestart;
+
+import it.polimi.ingsw.network.messages.server.ServerMessage;
+
+public class StopWaiting extends ServerMessage {
+
+    public StopWaiting() {
+    }
+
+}
--- codex_naturalis/src/main/java/it/polimi/ingsw/network/messages/server/login/PlayersAndColorPins.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/network/messages/server/login/PlayersAndColorPins.java
@@ -2,27 +2,26 @@ package it.polimi.ingsw.network.messages.server.login;
 
 import it.polimi.ingsw.controller.client.Controller;
 import it.polimi.ingsw.model.Color;
-import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.network.messages.server.ServerMessage;
 
 import java.util.HashMap;
 
 public class PlayersAndColorPins extends ServerMessage {
-   private final HashMap<Player,Color> map;
+    private final HashMap<String, Color> map;
 
-    public PlayersAndColorPins(HashMap<Player, Color> map) {
+    public PlayersAndColorPins(HashMap<String, Color> map) {
         this.map = map;
     }
 
-    public HashMap<Player, Color> getMap() {
+    public HashMap<String, Color> getMap() {
         return map;
     }
 
     @Override
     public void callController(Controller controller) {
-        HashMap<String,Color> newMap = new HashMap<>();
-        for(Player p: map.keySet()){
-            newMap.put(p.getName(), map.get(p));
+        HashMap<String, Color> newMap = new HashMap<>();
+        for (String p : map.keySet()) {
+            newMap.put(p, map.get(p));
         }
         controller.refreshUsers(newMap);
     }
