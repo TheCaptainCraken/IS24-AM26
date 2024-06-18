92bb132da986a20c24cff4384cebe9cc70f9213d
pietrobenecchi <pietro.benecchi@gmail.com>
2024-06-18 10:00:51 +0200

    create different version for file jar

--- codex_naturalis/src/main/java/it/polimi/ingsw/App.java
+++ codex_naturalis/src/main/java/it/polimi/ingsw/App.java
@@ -1,79 +1,49 @@
 package it.polimi.ingsw;
 
-
 import it.polimi.ingsw.controller.client.Controller;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.util.Scanner;
 
-
-/**
- * JavaFX App
- */
-public class App{
-
-    private static Scene scene;
-    private Controller controller;
-
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
+public class App {
+    /**
+     * JavaFX App
+     */
     public static void main(String[] args) {
+        String connectionType = args[0];
+        String ip = args[1];
+        String port = args[2];
+        String viewType = args[1];
         Controller controller = null;
-        Scanner myObj = new Scanner(System.in);
 
-        String choice;
         controller = new Controller();
         System.out.println("Choice your type of connection: RMI or Socket");
 
-      do{
-          choice = myObj.nextLine();
+        switch(connectionType){
+                case "RMI":
+                case "Socket":
+                    controller.createInstanceOfConnection(connectionType);
+                    break;
+                default:
+                    System.out.println("Invalid choice, please insert RMI or Socket");
+                    System.exit(0);
+            }
 
-          switch(choice){
-              case "RMI":
-              case "Socket":
-                  controller.createInstanceOfConnection(choice);
-                  break;
-              default:
-                  System.out.println("Invalid choice, please insert RMI or Socket");
-          }
-      }while(!choice.equals("RMI") && !choice.equals("Socket"));
-
-       System.out.println("Choice your type of view: TUI or GUI");
-       do{
-           choice = myObj.nextLine();
-           switch(choice){
-               case "TUI":
-               case "GUI":
-                   try {
-                       controller.setView(choice);
-                   } catch (InterruptedException e) {
-                       System.out.println("Error in setting the view");
-                       System.exit(0);
-                   }
-                   break;
-               default:
-                   System.out.println("Invalid choice, please insert TUI or GUI");
-           }
-       }while(!choice.equals("TUI") && !choice.equals("GUI"));
+        switch(viewType){
+            case "TUI":
+            case "GUI":
+                try {
+                    controller.setView(viewType);
+                } catch (InterruptedException e) {
+                    System.out.println("Error in setting the view");
+                    System.exit(0);
+                }
+                break;
+            default:
+                System.out.println("Invalid choice, please insert TUI or GUI");
+                System.exit(0);
+        }
 
     }
 }
+
 
